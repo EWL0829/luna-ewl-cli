4596642f3d1287a0502c9660e491f4da59c6bd25
ewl <amoryvictorialee@outlook.com>
2023-02-22 21:15:27 +0800

    initial commit with change core and utils for testing

---
+++ packages/core/lib/core.js
@@ -0,0 +1,10 @@
+'use strict';
+const utils = require('@luna-ewl-cli/utils');
+module.exports = core;
+
+function core() {
+    // TODO
+    const u = utils();
+    console.log('u', u); // eslint-disable-line
+    console.log('update core - 1.0.0'); // eslint-disable-line
+}
---
+++ packages/utils/lib/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = index;
+
+function index() {
+    // TODO
+    console.log('update utils - 1.0.0'); // eslint-disable-line
+}
